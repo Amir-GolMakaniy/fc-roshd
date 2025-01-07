83b97a6552b21419bab456a0b25ab93af9098448
Amir Goli <amirgolidev@gmail.com>
2025-01-07 22:54:51 +0330

    add icon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title ?? 'Page Title' }}</title>
+    <link rel="shortcut icon" href="{{ asset('images/THE-ROSHD.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.rtl.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link rel="stylesheet" href="{{ asset('css/persian.datepicker.css') }}"/>
